0ef99b3a0bdc22aca9ddb64ce42209290cdd57fa
the.xerx@gmail.com <the.xerx@gmail.com@81cb7436-523d-0410-973d-2dae8de84927>
2013-08-21 20:09:10 +0000

    - statistics server

---
+++ RockMargin/HTTPClient/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HTTPClient
+{
+	class Program
+	{
+		static MonitoringService monitoring = new MonitoringService();
+
+		static void Crash()
+		{
+			WebRequest a = null;
+			a.Abort();
+		}
+
+		static void Main(string[] args)
+		{
+			AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
+
+			Thread.Sleep(2000);
+			Crash();
+		}
+
+		static void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+		{
+			var ex = e.ExceptionObject as Exception;
+			if (ex != null)
+			{
+				monitoring.SendCrashReport(ex.StackTrace);
+			}
+		}
+	}
+}
---
+++ RockMargin/HTTPServer/Storage.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Xml;
+
+namespace HTTPServer
+{
+	class Storage
+	{
+		private readonly string STORAGE_FILE = "storage.xml";
+		private readonly TimeSpan PERSISTANCE_INTERVAL = new TimeSpan(0, 0, 1);
+
+		private DateTime _lastSave = DateTime.MinValue;
+
+		public struct Entry
+		{
+			public UserInfo user;
+			public DateTime date;
+
+			public Entry(UserInfo user)
+			{
+				this.user = user;
+				this.date = DateTime.Now;
+			}
+
+			public void Serialize(XmlElement node)
+			{
+				user.SerializeToXml(node);
+				node.SetAttribute("date", date.ToString());
+			}
+
+			public static Entry Deserialize(XmlElement node)
+			{
+				return new Entry {
+					user = new UserInfo(node),
+					date = DateTime.Parse(node.GetAttribute("date"))
+				};
+			}
+		}
+
+		public List<Entry> PresenceReports { get; private set; }
+		public Dictionary<Entry, string> CrashReports { get; private set; }
+
+		public Storage()
+		{
+			PresenceReports = new List<Entry>();
+			CrashReports = new Dictionary<Entry, string>();
+
+			Load();
+		}
+				
+		public void AddPresenceReport(UserInfo user)
+		{
+			bool exists = PresenceReports.Exists((Entry e) =>
+				e.user.IPAddress == user.IPAddress &&
+				e.user.MachineName == user.MachineName &&
+				e.date.Date == DateTime.Today);
+
+			if (!exists)
+			{
+				PresenceReports.Add(new Entry(user));
+				PersistanceCheckpoint();
+			}
+		}
+
+		public void AddCrashReport(UserInfo user, string callstack)
+		{
+			var entry = new Entry(user);
+			if (!CrashReports.ContainsKey(entry))
+			{
+				CrashReports.Add(entry, callstack);
+				PersistanceCheckpoint();
+			}
+		}
+
+		private void PersistanceCheckpoint()
+		{
+			if (DateTime.Now > _lastSave.Add(PERSISTANCE_INTERVAL))
+			{
+				Save();
+			}
+		}
+
+		public string GetXmlString()
+		{
+			XmlDocument doc = GenerateXml();
+			return doc.InnerXml;
+		}
+
+		private XmlDocument GenerateXml()
+		{
+			XmlDocument doc = new XmlDocument();
+			var root = doc.CreateElement("storage");
+
+			foreach (var report in PresenceReports)
+			{
+				var node = doc.CreateElement("presence");
+				report.Serialize(node);
+				root.AppendChild(node);
+			}
+
+			foreach (var report in CrashReports)
+			{
+				var node = doc.CreateElement("crash");
+				report.Key.Serialize(node);
+				node.AppendChild(doc.CreateTextNode(report.Value));
+				root.AppendChild(node);
+			}
+
+			doc.AppendChild(root);
+			return doc;
+		}
+
+		private void Save()
+		{
+			XmlDocument doc = GenerateXml();
+			doc.Save(STORAGE_FILE);
+			_lastSave = DateTime.Now;
+		}
+
+		private void Load()
+		{
+			PresenceReports.Clear();
+			CrashReports.Clear();
+
+			XmlDocument doc = new XmlDocument();
+			try
+			{
+				doc.Load(STORAGE_FILE);
+				foreach (XmlElement node in doc.DocumentElement.ChildNodes)
+				{
+					Entry entry = Entry.Deserialize(node);
+					
+					if (!entry.user.IsValid())
+						continue;
+
+					switch (node.Name)
+					{
+						case "presence":
+							PresenceReports.Add(entry);
+							break;
+
+						case "crash":
+							CrashReports.Add(entry, node.InnerText);
+							break;
+					}
+				}
+			}
+			catch (FileNotFoundException) { }
+		}
+	}
+}
