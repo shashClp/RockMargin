d1a4156c24efe9359612801194fbf12a4e560656
the.xerx <the.xerx@gmail.com>
2013-08-28 22:18:09 +0000

    - monitoring service

---
+++ RockMargin/Core/IPLookup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RockMargin
+{
+	class IPLookup
+	{
+		private readonly string LOOKUP_SERVER = "http://checkip.org";
+
+		public string IPAddress { get; private set; }
+		public string Country { get; private set; }
+		public string City { get; private set; }
+
+		public IPLookup()
+		{
+			var web_client = new WebClient();
+			string data = web_client.DownloadString(LOOKUP_SERVER);
+
+			IPAddress = FindInfo(data, @">IP Address: .*?>(\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b)");
+			Country = FindInfo(data, @"Country: (.*?)<");
+			City = FindInfo(data, @"City: (.*?)<");
+		}
+
+		private string FindInfo(string source, string pattern)
+		{
+			var regex = new Regex(pattern);
+			var match = regex.Match(source);
+			return (match.Success && match.Groups.Count > 1) ? match.Groups[1].Value : string.Empty;
+		}
+	}
+}
---
+++ RockMargin/Core/MonitoringService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Timers;
+using System.Xml;
+
+namespace RockMargin
+{
+	static class MonitoringService
+	{
+		static readonly string MONITORING_SERVER = "92.249.114.70";
+
+		static private HttpWebRequest CreateRequest(string request_type)
+		{
+			string uri = string.Format("http://{0}/{1}/", MONITORING_SERVER, request_type);
+			var request = WebRequest.Create(uri) as HttpWebRequest;
+			request.KeepAlive = false;
+
+			request.CookieContainer = new CookieContainer();
+			request.CookieContainer.Add(new Uri(uri), new Cookie("vsix_version", Utils.GetVsixVersion()));
+			request.CookieContainer.Add(new Uri(uri), new Cookie("vs_version", Utils.VSVersion));
+			request.CookieContainer.Add(new Uri(uri), new Cookie("machine_name", Environment.MachineName));
+
+			return request;
+		}
+
+		static private void GetResponseAsync(HttpWebRequest request)
+		{
+			var thread = new Thread(() =>
+			{
+				try
+				{
+					var ip_lookup = new IPLookup();
+					request.CookieContainer.Add(request.RequestUri, new Cookie("ip", ip_lookup.IPAddress));
+					request.CookieContainer.Add(request.RequestUri, new Cookie("country", ip_lookup.Country));
+					request.CookieContainer.Add(request.RequestUri, new Cookie("city", ip_lookup.City));
+
+					request.GetResponse();
+				}
+				catch {}
+			});
+			thread.Start();
+		}
+
+		static public void SendPresense()
+		{
+			var request = CreateRequest("presence");
+			GetResponseAsync(request);
+		}
+
+		static public void SendCrashReport(string callstack)
+		{
+			var request = CreateRequest("crash");
+
+			byte[] buffer = System.Text.Encoding.UTF8.GetBytes(callstack);
+			request.ContentType = "text/xml";
+			request.Method = "POST";
+			request.ContentLength = buffer.Length;
+
+			try
+			{
+				var stream = request.GetRequestStream();
+				stream.Write(buffer, 0, buffer.Length);
+				GetResponseAsync(request);
+			}
+			catch { }
+		}
+	}
+}
