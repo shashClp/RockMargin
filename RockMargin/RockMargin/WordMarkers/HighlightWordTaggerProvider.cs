182a60f19d9a98598b826ebec78cf0a16e3c16a9
the.xerx@gmail.com <the.xerx@gmail.com@81cb7436-523d-0410-973d-2dae8de84927>
2013-08-28 22:18:09 +0000

    - monitoring service

--- RockMargin/RockMargin/WordMarkers/HighlightWordTaggerProvider.cs
+++ RockMargin/RockMargin/WordMarkers/HighlightWordTaggerProvider.cs
@@ -9,12 +9,15 @@ using Microsoft.VisualStudio.Text.Operations;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
 
 namespace RockMargin
 {
 	[Export(typeof(IViewTaggerProvider))]
-	[ContentTypeAttribute("any")]
+	[ContentTypeAttribute("text")]
 	[TagType(typeof(TextMarkerTag))]
+	[TextViewRole(PredefinedTextViewRoles.Document)]
 	class HighlightWordTaggerProvider: IViewTaggerProvider
 	{
 		[Import]
@@ -26,6 +29,9 @@ namespace RockMargin
 		[Import]
 		internal IEditorFormatMapService FormatMapService { get; set; }
 
+		[Import]
+		IVsEditorAdaptersFactoryService AdaptersService { get; set; }
+
 
 		public ITagger<T> CreateTagger<T>(ITextView view, ITextBuffer buffer) where T : ITag
 		{
@@ -35,7 +41,12 @@ namespace RockMargin
 
 			var navigator = TextStructureNavigatorService.GetTextStructureNavigator(buffer);
 			var format = FormatMapService.GetEditorFormatMap(view);
-			return HighlightWordTagger.Instance(view as IWpfTextView, format, TextSearchService, navigator) as ITagger<T>;
+			var tagger = HighlightWordTagger.Instance(view as IWpfTextView, format, TextSearchService, navigator);
+
+			IVsTextView view_adapter = AdaptersService.GetViewAdapter(view);
+			new CommandFilter(view_adapter, tagger);
+
+			return tagger as ITagger<T>;
 		}
 	}
 }
