cfb0090ede722f93feed7b443c4b474c27760486
Kitty <the.xerx@gmail.com>
2018-01-12 00:18:12 +0200

    Remove unused usings

--- RockMargin/Marks/ColorExtractor.cs
+++ RockMargin/Marks/ColorExtractor.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 using System.Windows.Media;
 using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
