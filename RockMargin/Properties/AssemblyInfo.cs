205dce1338f531af197a902f6ec30bd34b51715d
Kitty <the.xerx@gmail.com>
2018-01-13 23:20:09 +0200

    Updated AssemblyInfo properties

--- RockMargin/Properties/AssemblyInfo.cs
+++ RockMargin/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RockMargin")]
+[assembly: AssemblyTitle("")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RockMargin")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyProduct("RockMargin™")]
+[assembly: AssemblyCopyright("© 2012 Kitty")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
